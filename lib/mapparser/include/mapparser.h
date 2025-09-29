ad103dd2e8bdeb2d29819fface82a4cfa2ca1ca5
Morgan <morgan.guichard@epitech.eu>
2025-09-29 11:48:26 +0200

    feat: initial commit of map parser to declare the lib

---
+++ lib/mapparser/include/mapparser.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype
+** File description:
+** TODO: add description
+*/
+#ifndef MAPPARSER_H
+#define MAPPARSER_H
+
+#include <vector>
+#include <memory>
+
+class MapParser {
+public:
+    MapParser();
+    ~MapParser(); // Add destructor declaration
+    void clean();
+};
+
+#endif //MAPPARSER_H
---
+++ lib/mapparser/src/mapparser.cpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2025
+** rtype
+** File description:
+** TODO: add description
+*/
+
+#include "mapparser.h"
+#include <cstring>
+#include <memory>
+#include <stdexcept>
+#include <algorithm>
+
+MapParser::MapParser() {
+}
+
+// Add destructor to clean up memory
+MapParser::~MapParser() {
+    clean();
+}
+
+void MapParser::clean() {
+    return;
+}
