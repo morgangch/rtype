f89d5909d6e76d99653caab75c549ce1f1c2ad10
Lucas-Epitech <lucas.carre@epitech.eu>
2025-09-29 15:24:02 +0200

    feat graphic: add base graphic system with enemy player and input

--- common/components/Position.cpp
+++
@@ -1,6 +0,0 @@
-#include "common/components/Position.hpp"
-
-namespace rtype::common::components {
-    Position::Position(float x, float y, float rotation)
-        : x(x), y(y), rotation(rotation) {}
-}
