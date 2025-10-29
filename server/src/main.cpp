5296b26c7a6a3e205d18269f14be8cd509e561cf
Eliot <73363100+EliotAmn@users.noreply.github.com>
2025-10-29 14:32:53 +0100

    feat: implement the threaded architecture on the server (#239)
    * feat: add mutexes for thread-safe packet management in PacketManager
    
    * Revert "feat: add mutexes for thread-safe packet management in PacketManager"
    
    This reverts commit acde5de376dc517791ed1c048025a11250da6bd6.
    
    * feat: implement thread-safe PacketManager with mutex protection
    
    * feat: run network loop in a separate thread for improved performance
    
    * refactor: update PacketManager test assertions to use size() method directly

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -4,7 +4,6 @@
 #include "controllers/RoomController.h"
 #include "systems/PacketHandlingSystem.h"
 #include "systems/ServerEnemySystem.h"
-#include "systems/ServerEntityCleanupSystem.h"
 #include "systems/ServerCollisionSystem.h"
 #include <common/components/Position.h>
 #include <common/components/Velocity.h>
@@ -14,6 +13,7 @@
 #include <iostream>
 #include <cmath>
 #include "systems/AdminDetectorSystem.h"
+#include <thread>
 
 // Game bounds - must match client SCREEN_WIDTH and SCREEN_HEIGHT
 constexpr float GAME_WIDTH = 1280.0f;
@@ -41,10 +41,13 @@ void clampPlayerPosition(rtype::common::components::Position* pos) {
         pos->y = GAME_HEIGHT - PLAYER_HALF_SIZE;
     }
 }
-
+void net_loop() {
+    while (true) {
+        rtype::server::network::loop_recv(root.udp_server_fd);
+        rtype::server::network::loop_send(root.udp_server_fd);
+    }
+}
 void rtype::server::Rtype::loop(float deltaTime) {
-    network::loop_recv(udp_server_fd);
-    network::loop_send(udp_server_fd);
     packetHandler.processPackets(packetManager.fetchReceivedPackets());
     
     // Simple server-side movement system (update positions based on velocity)
@@ -85,6 +88,9 @@ void rtype::server::Rtype::loop(float deltaTime) {
 int main() {
     rtype::server::Rtype &r = root;
     r.udp_server_fd = rtype::server::network::setupUDPServer(4242);
+    // Run the network loop in a separate thread
+    std::thread networkThread(net_loop);
+    networkThread.detach();
 
     root.packetHandler.registerCallback(Packets::JOIN_ROOM, rtype::server::controllers::room_controller::handleJoinRoomPacket);
     root.packetHandler.registerCallback(Packets::GAME_START_REQUEST, rtype::server::controllers::room_controller::handleGameStartRequest);
